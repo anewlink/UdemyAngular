15617ae0d21f6bb911de177dc13f9469a76ff039
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-10-29 14:28:01 -0500

    1. assignment 2 finished

--- Assignments/1/basics-assignment-2-start/src/app/app.component.ts
+++ Assignments/1/basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username: string = '';
+  resetUsername() {
+    this.username = '';
+  }
+  isUsernameSet() {
+    return this.username.length > 0;
+  }
 }
