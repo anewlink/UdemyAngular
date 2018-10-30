c8f8d93484caf01694ffbdb32050057c65dcf372
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-10-30 16:03:51 -0500

    Recipe project ready to start

--- recipe-and-shopping/src/app/app.component.ts
+++ recipe-and-shopping/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
 
+=======
+  title = 'recipe-and-shopping';
+>>>>>>> Recipe project ready to start
 }
