fdb6ba77bac99846ea8ebcf1b888cd1fd0699313
Yeison Del Rio <ydelriov@psl.com.co>
2018-10-31 14:41:07 -0500

    Added components

--- recipe-and-shopping/src/app/app.module.ts
+++ recipe-and-shopping/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { IngredientComponent } from './header/shopping-list/ingredient/ingredient.component';
+import { RecipeComponent } from './header/recipe-book/recipe-list/recipe/recipe.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    ShoppingListComponent,
+    IngredientComponent,
+    RecipeComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
