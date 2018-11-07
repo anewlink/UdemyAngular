9e21b938c960915e7b780dbe6615d7989e997d3c
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-11-06 21:09:42 -0500

    5. ngBinding assigment

--- Assignments/5/cmp-databinding-assignment-start/src/app/app.component.ts
+++ Assignments/5/cmp-databinding-assignment-start/src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(eventNumber: number) {
+    //console.log("numberCreated", eventNumber);
+    if(eventNumber%2 === 0){
+      this.evenNumbers.push(eventNumber);
+    }else{
+      this.oddNumbers.push(eventNumber);
+    }
+  }
+
 }
--- cmp-databinding/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
-=======
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
->>>>>>> 0f8cd57e3a53de062da343596611ce9054c1586e
 
 @Component({
   selector: 'app-cockpit',
@@ -12,23 +8,16 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-<<<<<<< HEAD
   // newServerName = '';
   // newServerContent = '';
 
   @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
-=======
-  newServerName = '';
-  newServerContent = '';
-
->>>>>>> 0f8cd57e3a53de062da343596611ce9054c1586e
   constructor() { }
 
   ngOnInit() {
   }
 
-<<<<<<< HEAD
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
@@ -43,25 +32,6 @@ export class CockpitComponent implements OnInit {
       serverContent: this.serverContentInput.nativeElement.value
       // serverContent: this.newServerContent
     });
-=======
-  onAddServer() {
-    this.serverCreated.emit(
-      {
-        serverName: this.newServerName,
-        serverContent: this.newServerContent
-      }
-    );
-  }
-
-  onAddBlueprint() {
-    this.blueprintCreated.emit(
-      {
-        serverName: this.newServerName,
-        serverContent: this.newServerContent
-      }
-    );
-
->>>>>>> 0f8cd57e3a53de062da343596611ce9054c1586e
   }
 
 
