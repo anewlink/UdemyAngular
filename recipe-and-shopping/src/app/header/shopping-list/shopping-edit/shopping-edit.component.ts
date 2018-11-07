ca328da223ee95379a8bd25f2518bdd2c200a7cf
Yeison Del Rio <ydelriov@psl.com.co>
2018-11-07 18:56:22 -0500

    6.Adding a item to cart: not working

--- recipe-and-shopping/src/app/header/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-and-shopping/src/app/header/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredienteAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    console.log('onAddItem');
+    const newIngredient = { name: this.nameInputRef.nativeElement.value, amount: this.amountInputRef.nativeElement.value };
+    console.log(newIngredient);
+    this.ingredienteAdded.emit(newIngredient);
   }
 
 }
