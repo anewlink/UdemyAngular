4230d7655b2a59f4dbf3a7932fe81f840020803a
Gabriel Martinez <ext-gabriel.martinez@here.com>
2018-11-16 17:36:54 -0500

    10. partial assigment

--- Assignments/10/services-assignment-start/src/app/app.component.ts
+++ Assignments/10/services-assignment-start/src/app/app.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CounterService],
 })
 export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+  // activeUsers = ['Max', 'Anna'];
+  // inactiveUsers = ['Chris', 'Manu'];
+  activeUsers = this.usersService.activeUsers;
+  inactiveUsers = this.usersService.inactiveUsers;
 
   onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
+    this.usersService.setToInactive(id);
+    /* this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1); */
+    /* this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1); */
   }
 
   onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+    /* this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1); */
+    this.usersService.setToActive(id);
+  }
+
+  constructor(private usersService: UsersService) {
+
   }
 }
---
+++ Assignments/10/services-assignment-start/src/app/counter.service.ts
@@ -0,0 +1,10 @@
+export class CounterService {
+
+  userActivaterCounter = 2;
+  userDeactivaterCounter = 2;
+
+constructor() {
+
+}
+
+}
