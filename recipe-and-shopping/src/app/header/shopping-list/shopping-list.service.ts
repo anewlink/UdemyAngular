a63ef983666fe8096f5acf656de916e63d4c24b0
Gabriel Martinez <ext-gabriel.martinez@here.com>
2018-11-21 17:14:08 -0500

    Add ingredients to shopping list

--- recipe-and-shopping/src/app/header/shopping-list/shopping-list.service.ts
+++ recipe-and-shopping/src/app/header/shopping-list/shopping-list.service.ts
@@ -1,21 +1,38 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, OnInit } from "@angular/core";
 
-export class ShoppingListService {
+export class ShoppingListService implements OnInit {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
 
+  constructor() {}
+
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatos', 10)
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatos", 10)
   ];
 
-  addIngredient(ingredient: Ingredient){
+  addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+    /* for (const ingredient of ingredients) {
+      this.addIngredient(ingredient);
+    } */
   }
 
   getIngredients() {
     return this.ingredients.slice();
   }
 
+  ngOnInit() {
+/*     this.recipeService.recipeIngredientsAdded.subscribe(ingredientList => {
+      ingredientList.forEach(ingredient => {
+        this.addIngredient(ingredient);
+      });
+    }); */
+  }
 }
