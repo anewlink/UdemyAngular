0e5ae24bf0be4ec919dfca5e05e43aec756378e0
Gabriel Martinez <ext-gabriel.martinez@here.com>
2018-11-27 17:13:13 -0500

    13. added a subject. Work as a suscription and subscriber. It can replace EventEmitter

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, Observable, Observer } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    /* const myNumbers = interval(1000);
-    myNumbers.subscribe(
+    const myNumbers = interval(1000);
+    this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
       }
-    ); */
+    );
 
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
@@ -34,11 +36,16 @@ export class HomeComponent implements OnInit {
         // observer.error('this does not work');
       }, 6000);
     });
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => { console.log(data); },
       (error: string) => { console.log(error); },
       () => { console.log('completed'); }
     );
+  }
+
+  ngOnDestroy(): void {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
 
 }
