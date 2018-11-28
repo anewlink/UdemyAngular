316d9c667818966cd00cf5faa9d214f2cae05d27
Gabriel Martinez <ext-gabriel.martinez@here.com>
2018-11-28 17:20:45 -0500

    15. template forms assigment end

--- Assignments/15/forms-td-assignment/src/app/app.component.ts
+++ Assignments/15/forms-td-assignment/src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
+
   @ViewChild('f') customForm: NgForm;
 
   form = {
@@ -24,6 +26,8 @@ export class AppComponent implements OnInit {
     'Pro'
   ];
 
+  defaultSubscription = this.subscriptions[1];
+
   onSubmit(form) {
     this.form.emails.email1 = this.customForm.value.emails.email1;
     this.form.emails.email2 = this.customForm.value.emails.email2;
@@ -32,7 +36,7 @@ export class AppComponent implements OnInit {
     console.log(form, this.form);
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     console.log(this.customForm);
     this.customForm.form.patchValue({subscriptions: 'Advanced'});
   }
