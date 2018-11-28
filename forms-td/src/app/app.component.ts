d9bb665769020e64d8eb64c8dc458f33169f5927
Gabriel Martinez <ext-gabriel.martinez@here.com>
2018-11-28 14:48:43 -0500

    15. ngModel oneWayBinding

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { parseTemplate } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
+/*   onSubmit(form: NgForm) {
     console.log('Submitted!', form);
+  } */
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
