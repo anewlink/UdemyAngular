07a6048824fb37a30fdd78abd38b64c49aea49ae
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-12-02 18:50:26 -0500

    18. Added Http, Put

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ServerService } from "./server.service";
+import { Response } from "@angular/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  constructor(private serverService: ServerService) {}
   servers = [
     {
-      name: 'Testserver',
+      name: "Testserver",
       capacity: 10,
       id: this.generateId()
     },
     {
-      name: 'Liveserver',
+      name: "Liveserver",
       capacity: 100,
       id: this.generateId()
     }
@@ -24,6 +27,24 @@ export class AppComponent {
       capacity: 50,
       id: this.generateId()
     });
+  }
+  onSave() {
+    this.serverService
+      .storeServers(this.servers)
+      .subscribe(
+        response => console.log(response),
+        error => console.log(error)
+      );
+  }
+  onGet() {
+    this.serverService.getServers()
+      .subscribe(
+        (response: Response) => {
+          const data = response.json()
+          console.log(data);
+        },
+        error => console.log(error)
+      );
   }
   private generateId() {
     return Math.round(Math.random() * 10000);
---
+++ http/src/app/server.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+@Injectable()
+export class ServerService {
+  constructor(private http: Http) {}
+
+  storeServers(servers: any[]) {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+/*     return this.http.post(
+      'https://udemy-ng-http-e47b0.firebaseio.com/' + 'data.json',
+      servers,
+      { headers: headers }
+    ); */
+    return this.http.put(
+      'https://udemy-ng-http-e47b0.firebaseio.com/' + 'data.json',
+      servers,
+      { headers: headers }
+    );
+  }
+  getServers() {
+    return this.http.get('https://udemy-ng-http-e47b0.firebaseio.com/' + 'data.json');
+  }
+}
