460e35455ca12b8fcb60d0b5001199c40a57862a
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-12-15 12:17:42 -0500

    Added signuo effect

--- recipe-and-shopping/src/app/auth/signup/signup.component.ts
+++ recipe-and-shopping/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from '../auth.service';
+/* import { AuthService } from '../auth.service'; */
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import * as AuthActions from '../store/auth.actions';
 
 @Component({
   selector: 'app-signup',
@@ -9,7 +12,7 @@ import { AuthService } from '../auth.service';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private store: Store<fromApp.AppState>/* private authService: AuthService */) { }
 
   ngOnInit() {
   }
@@ -18,7 +21,8 @@ export class SignupComponent implements OnInit {
     console.log("sign up1");
     const email = form.value.email;
     const password = form.value.password;
-    this.authService.signupUser(email, password);
+    //this.authService.signupUser(email, password);
+    this.store.dispatch(new AuthActions.TrySignup({username: email, password: password}));
   }
 
 }
--- recipe-and-shopping/src/app/auth/store/auth.effects.ts
+++ recipe-and-shopping/src/app/auth/store/auth.effects.ts
@@ -1,10 +1,40 @@
-import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Injectable } from "@angular/core";
+import { Effect, Actions } from "@ngrx/effects";
+import { map, switchMap, mergeMap } from "rxjs/operators";
+import { from } from "rxjs";
+import * as firebase from "firebase";
+import * as AuthActions from './auth.actions';
 
 @Injectable()
 export class AuthEffects {
-  @Effect()
-  authSignup;
+  @Effect() //{dispatch: false } if the effect don't imply an state change
+  authSignup = this.actions$
+    .ofType(AuthActions.TRY_SIGNUP)
+    .pipe(
+      map((action: AuthActions.TrySignup) => {
+        return action.payload;
+      }),
+      switchMap((authData: {username: string, password: string}) => {
+        return from(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password))
+      }),
+      switchMap(() => {
+        return from(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        return [
+          {
+            type: AuthActions.SIGNUP
+          },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          }
+        ];
+      })
+    );
+    /* .map((action: AuthActions.TrySignup) => {
+      return action.payload
+    }).switchMap(); */
 
   constructor(private actions$: Actions){
 
