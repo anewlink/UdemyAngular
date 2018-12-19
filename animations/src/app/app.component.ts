175c1f0022db514688101310d0bdb8c8ff6f1daa
Gabriel Martinez Rojas <gabriel.martinez.rojas@gmail.com>
2018-12-19 01:33:16 -0500

    26. More advanced transition

--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -7,24 +7,53 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   animations: [
     trigger('divState', [
       state('normal', style({
-        'background-color': 'red',
+        backgroundColor: 'red',
+        /* 'background-color': 'red', */
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('highlighted <=> normal', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        backgroundColor: 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(100px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500)
+      /* animate(500, style({
+        borderRadius: '50px'
+      } ))*/), //animate('150ms ease-in-out')
+
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
+    console.log('onAnimate');
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    console.log('onShrink');
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
@@ -34,4 +63,6 @@ export class AppComponent {
   onDelete(item) {
     this.list.splice(this.list.indexOf(item), 1);
   }
+
+
 }
